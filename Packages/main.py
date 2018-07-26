7a1f4671458e4d85cf60b2729c61543617e6e357
Cameron <cameronmeissner@gmail.com>
2018-07-25 22:11:18 -0400

    New Features
    Removed second try/except block

--- Packages/main.py
+++ Packages/main.py
@@ -6,6 +6,8 @@ user = c.windll.user32
 
 SCREEN_WIDTH = user.GetSystemMetrics(0) 
 SCREEN_HEIGHT = user.GetSystemMetrics(1)
+CURRENT_PATH = os.getcwd()
+MAX_FILENAME_LENGTH = 170
 
 #post: If there is no file present containg a target location, a new file is made
 # and the path is written. Will read the path from the file and return it as a string
@@ -27,20 +29,17 @@ def create_target_path(path):
 
 #post: logs time of current execution to a log file on startup
 def log(reddits, dls, errs):
-    if not os.path.exists('log.txt'):
-        mode = 'w'
-    else:
-        mode = 'a'
-    f = open('log.txt', mode)
+    mode = 'w' if not os.path.exists('log.txt') else 'a'
     reddits = reddits.replace(' ', '')
     reddits = reddits.replace(',', ', ')
     current_time = time.asctime(time.localtime(time.time()))
-    f.write(current_time + '\n')
-    f.write('Subreddits searched: ' + str(reddits) + '\n')
-    f.write('Images downloaded: ' + str(dls) + '\n')
-    f.write('Encountered errors: ' + errs + '\n')
-    f.write('--------------------------------\n')
-    f.close()
+    with open('log.txt', mode) as f:
+        f.write(current_time + '\n')
+        f.write('Subreddits searched: ' + str(reddits) + '\n')
+        f.write('Images downloaded: ' + str(dls) + '\n')
+        f.write('Encountered errors: ' + errs + '\n')
+        f.write('--------------------------------\n')
+        f.close()
 
 #post: Returns true if argument is of the float class, false otherwise
 def is_float(s):
@@ -52,10 +51,10 @@ def is_float(s):
 
 #post: Returns the resolution embedded in a reddit post title in (width, height) form
 def get_resolution(title):
-    delimiters = ['[',']','(',')']
+    delims = ['[',']','(',')']
     width, height = '', ''
     res = []
-    for char in delimiters:
+    for char in delims:
         if char in title:
             title = title.replace(char, '')
     title = list(title)
@@ -71,11 +70,15 @@ def get_resolution(title):
             width, height = 0, 0
     return int(width), int(height)
 
+
 #post: Returns true if the resolution in the post title is at least 1920x1080 and if width > height
 #returns false otherwise
-def compatable(post):
+def compatable(path, post):
     width, height = get_resolution(post.title)
-    return width >= SCREEN_WIDTH and height >= SCREEN_HEIGHT and width > height
+    flickr_image = 'https://www.flickr.com/' in post.url
+    on_disk = os.path.exists(os.path.join(path, cleanup_title(post.title)))
+    correct_dimensions = width >= SCREEN_WIDTH and height >= SCREEN_HEIGHT and width > height
+    return not flickr_image and not on_disk and correct_dimensions
 
 #post: removes problematic characters from a reddit post title for parsing,
 # returns a new cleaned up string
@@ -84,7 +87,7 @@ def cleanup_title(text):
     for char in chars:
         if char in text:
             text = text.replace(char, '')
-    return text
+    return text + '.jpg'
 
 #post: appends .jpg the image url if it isn't already present, returns a new string 
 def cleanup_url(url):
@@ -105,16 +108,14 @@ def size_filter(main_path):
             dels += 1
     return dels
 
-#pre: subreddits is an array of subreddit strings without spaces, throws an exception otherwise
+#pre: subreddits is an array of subreddit strings, throws an exception otherwise
 #pre: target_path is a valid directory, throws and exception if otherwise
 #post: downloads all compatable images from the passed subreddits, searches for a max of max_images from
 #each subreddit. All images are placed in the target_path destination at the end of runtime
 def get_images(subreddits, _max, target_path):
-        httpErrors = 0
-        fileErrors = 0
+        errors = 0
         downloads = 0
-        good_posts = []
-        current_path = os.getcwd()
+        compatable_posts = []
         reddit = praw.Reddit(
                  client_id='TB0-ZpHZQF6Qog',
                  client_secret='Rz4XNGcDBZVMQE45IpB0EBl-p3s',
@@ -126,61 +127,56 @@ def get_images(subreddits, _max, target_path):
             if 'porn' not in sub:
                 sub += 'porn'
             for post in reddit.subreddit(sub).hot(limit=int(_max)):
-                if compatable(post):
-                    good_posts.append(post)
+                if compatable(target_path, post):
+                    compatable_posts.append(post)
 
-        if len(good_posts) == 0:
+        if len(compatable_posts) == 0:
             print('No compatable images found') 
             sys.exit()
             
-        print(str(len(good_posts)) + ' images detected')
-        print('Collecting images...')
+        print(str(len(compatable_posts)) + ' compatable images detected')
+        print('Collecting images...\n')
         time.sleep(1)
 
-        for post in good_posts:
-            file_name = cleanup_title(post.title) + '.jpg'
-            stored_file = os.path.join(target_path, file_name)
-            raw_file = os.path.join(current_path, file_name)
-            if not os.path.exists(stored_file): 
-                try:
-                    _file = open(file_name,'wb')
-                except FileNotFoundError as e:
-                    print('File not found')
-                    fileErrors += 1 
-                    continue
-                try:
-                    _file.write(web.urlopen(cleanup_url(post.url)).read())
-                    _file.close()
-                    os.rename(raw_file, stored_file)
-                    downloads += 1
-                except Exception:
-                    print('Unable to write file')
-                    _file.close()
-                    os.remove(raw_file)
-                    fileErrors += 1
-                print(file_name.encode('ascii', 'ignore').decode() + ' (' + str(post.ups) + ' upvotes) written') 
-            else:
-                print('Image already exists')
+        for post in compatable_posts:
+            filename = cleanup_title(post.title)
+            if len(filename) >= MAX_FILENAME_LENGTH:
+                print('Image name is too long, renaming file...')
+                mid = int(len(filename) / 2)
+                filename = filename[:mid] + '.jpg'
+            stored_file = os.path.join(target_path, filename)
+            raw_file = os.path.join(CURRENT_PATH, filename)  
+            _file = open(filename, 'wb')  
+            try:
+                _file.write(web.urlopen(cleanup_url(post.url)).read())
+                _file.close()
+                os.rename(raw_file, stored_file)
+                downloads += 1
+            except Exception:
+                print('Unable to write file: ' + filename + ' (' + post.url + ')')
+                _file.close()
+                os.remove(raw_file)
+                errors += 1
+            print(filename.encode('ascii', 'ignore').decode() + ' (' + str(post.ups) + ' upvote(s)) written') 
 
         dels = size_filter(target_path)
         new_download_count = downloads - dels
-        print(str(new_download_count) + ' new image(s) downloaded')
-        print(str(dels) + ' image(s) truncated due to a file size < 300KB')
-        errors = 'File/HTTP Errors: {}, String Encoding Errors: {}'.format(str(fileErrors),str(encodeErrors))
+        print('\n' + str(new_download_count) + ' new image(s) downloaded')
+        if dels > 0: print(str(dels) + ' image(s) smaller than 300KB removed')
+        errors = 'HTTP Errors: {}'.format(str(errors))
         print(errors)
         print('Opening image directory and exiting...')
         log(subreddits, new_download_count, errors)
-        time.sleep(1.5)
+        time.sleep(1.75)
         os.startfile(target_path)
 
-#####################################################
-#Main----->
+#Main--
 
 #Getting required input from prompts
 subs = input("Enter subreddit(s) to pull images from spereated by commas: ")
-_max = input("Enter max number of images to consider from each subreddit: ")
+_max = input("Enter number of images to scan from each subreddit: ")
         
-#Call to get_images with required input
+#Call to get_images with input
 get_images(subs, _max, get_target_path())
 
 
